15312c6db9098339021b5c5bde2fa2b574685380
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-06-03 06:55:17 +0300

    Now using PostgreSQL

--- application/__init__.py
+++ application/__init__.py
@@ -1,30 +1,35 @@
 from flask import Flask, session, request
 app = Flask(__name__)
 
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tsoha18.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tsoha18.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
 db = SQLAlchemy(app)
 
+
 from application import views
+from application import models
+
 from application.auth import views
+from application.auth import models
+
 from application.edit import views
 from application.admin import views
 
-from application.auth import models
-from application import models
-
-db.create_all()
-
-
-# Authentication
 
 from application.auth.models import User
-
 from flask_login import LoginManager
+from os import urandom
+
+app.config["SECRET_KEY"] = urandom(32)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -36,13 +41,14 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-# Populate database
 
-from sqlalchemy import exists
 from application.auth.models import Role
+from application.models import Company
+from application.auth.models import UserRole
 
-db_populated = db.session.query(exists().where(Role.id==1)).scalar() # We want to create data only on the first run
-if not db_populated:
+
+try:
+    db.create_all()
 
     role1 = Role('Administrator')
     role2 = Role('Editor')
@@ -51,13 +57,11 @@ if not db_populated:
     db.session.add(role2)
     db.session.add(role3)
 
-    from application.models import Company
     comp2 = Company("Aukustin asianajotoimisto Ky",2) # agreementlevel: 1 = Pro, 2 = Basic
     comp3 = Company("Idan ideahautomo",1)
     db.session.add(comp2)
     db.session.add(comp3)
 
-    from application.auth.models import User
     user1 = User('paivio@tsoha18','Päiviö','Pääkäyttäjä','salainen',None,True)
     user2 = User('yngve@tsoha18','Yngve','Ylläpitäjä','salainen',None,True)
     user3 = User('aukusti@asianajotoimisto','Aukusti','Asiakas','salainen',2,True)
@@ -69,7 +73,6 @@ if not db_populated:
     db.session.add(user4)
     db.session.add(user5)
 
-    from application.auth.models import UserRole
     userrole1 = UserRole(1, 1) # Administrator-paivio
     userrole2 = UserRole(2, 1) # Editor-paivio
     userrole3 = UserRole(2, 2) # Editor-yngve
@@ -84,4 +87,7 @@ if not db_populated:
     db.session.add(userrole5)
 
     db.session.commit()
+
+except:
+    pass
 
